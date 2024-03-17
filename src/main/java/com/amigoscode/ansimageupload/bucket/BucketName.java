aefc594140143257c6c0d435a6d91a667d2d634d
meryiir <mariemboujja14@gmail.com>
2024-03-17 06:40:39 +0000

    create S3 bucket via Amazon console

---
+++ src/main/java/com/amigoscode/ansimageupload/bucket/BucketName.java
@@ -0,0 +1,15 @@
+package com.amigoscode.ansimageupload.bucket;
+
+public enum BucketName {
+    PROFILE_IMAGE("amigoscode");/*name or image of the bucket created in amazon s3 client*/
+    private final String bucketName;
+
+    BucketName(String bucketName) {
+        this.bucketName = bucketName;
+
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+}
